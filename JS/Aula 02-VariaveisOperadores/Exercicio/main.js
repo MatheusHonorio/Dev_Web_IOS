6379b14a6a37fc7c10556864b228a4a89ff58e6c
MatheusHonorio <maat.teteu@hotmail.com>
2022-04-27 20:52:22 -0300

    Exercicio 2 javascript

---
+++ JS/Aula 02-VariaveisOperadores/Exercicio/main.js
@@ -0,0 +1,6 @@
+var pessoa = 'Matheus G';
+var Altura = 1.67;
+var Peso = 59;
+var imc  = Peso / (Altura ^2); 
+console.log('Meu nome é ' + pessoa + ' e eu tenho ' + Altura + ' de altura e peso ' + Peso + 'KG. Meu imc seria = ' +imc+'');
+
