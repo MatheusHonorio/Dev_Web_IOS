1dd7c9d801d73323b97f89c47152e843c2e1231c
Joyce <joyce.emailprofissional@gmail.com>
2022-05-02 21:55:41 -0300

    Exercicios JS em aula, 3 e 4

---
+++ JS/Aula 04/Funções/main.js
@@ -0,0 +1,51 @@
+// //Estrutura de uma função
+// /*
+// Fuction nomeDaFunção (valor1,valor2) {
+//   return valor1 * vaolor2   
+// }
+// */
+
+//  //Funcoes
+//  function AddNums (num1 = 2, num2= 4) {
+//   return num1 + num2
+//  }
+
+// //  console.log(AddNums()); 
+// let x = AddNums(10,11);
+// console.log(x)
+
+// console.clear();
+
+//  //Criando Arrow Fuction
+// const hello = (primeiro = 2, segundo= 2) => {
+//  return primeiro * segundo
+// }
+
+// console.log(hello);
+
+
+// const somaAddNums = (num1 = 2, num2= 4) => {
+//     return num1 + num2
+// }
+
+// console.clear();
+
+// const aluno = {
+//     primeiroNome: 'Matheus',
+//     segundoNome: 'lost',
+//     idade: 19
+// }
+
+// console.clear();
+// Evento Onload para mostrar mensagem enquanto carrega a web
+function teste () {
+    console.log('Bem Vindo a Nossa pagina Campeão')
+}
+// let teste = () => {
+//     console.log('Bem Vindo a Nossa pagina Campeão')
+// }
+
+
+function eventClick () {
+    console.log ('Você clicou aqui, gente boa')
+}
