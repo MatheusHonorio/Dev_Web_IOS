62d46d91e774cbf964be2d13c0f80f542208da70
Matheus H Casa <maat.teteu@hotmail.com>
2022-05-08 20:00:39 -0300

    Exercicios Feitos

--- JS/Aula 07/Em Aula/main5.js
+++ JS/Aula 07/Em Aula/main5.js
@@ -2,4 +2,4 @@ let nome = 'Nora';
 const notas = [7, 5, 9];
 
 let notaFinal = (notas[0] * 2 + notas[1] * 3 + notas[2] * 5) / 10;
-console.log(`Nome: ${nome}Nota 1: ${notas[0]}Nota 2: ${notas[1]}Nota 3: ${notas[2]}Média: ${finalScore}`);
+console.log(`Nome: ${nome}Nota 1: ${notas[0]}Nota 2: ${notas[1]}Nota 3: ${notas[2]}Média: ${notaFinal}`);
