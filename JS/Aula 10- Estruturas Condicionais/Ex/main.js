853b8c26d0ecd9e0a33a2849e8da7322bfebbce7
Matheus <maat.teteu@hotmail.com>
2022-05-11 21:50:27 -0300

    Exercicio não feitos

---
+++ JS/Aula 10- Estruturas Condicionais/Ex/main.js
@@ -0,0 +1,30 @@
+let IOS = [
+  {
+    nome: "Jorge",
+    salario: 1500,
+    vinculo: "CLT",
+    salarioCmInss: salario % 9,
+  },
+  {
+    nome: "Josévaldo",
+    salario: 1300,
+    vinculo: "PJ",
+  },
+  {
+    nome: "Lucélia",
+    salario: 3900,
+    vinculo: "CLT",
+  },
+  {
+    nome: "Rosinaldo",
+    salario: 2500,
+    vinculo: "PJ",
+  },
+  {
+    Nome: "Catolino",
+    salario: 2100,
+    vinculo: "CLT",
+  },
+];
+
+console.log();
