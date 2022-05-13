19201fe9bb726f47465091f33e8f0860572b8c8a
Matheus <maat.teteu@hotmail.com>
2022-05-12 21:45:30 -0300

    Exercicio 11 Finalizado

---
+++ JS/Aula 11- Laços de Repetição/Ex/ex.js
@@ -0,0 +1,10 @@
+function tabuada (){
+    let multiplicador = 2
+    let contador = 1
+    while (contador <=10){
+        console.log(`${multiplicador} x ${contador} = ${multiplicador * contador} `)
+        contador++
+    }
+}
+
+tabuada()
---
+++ JS/Aula 11- Laços de Repetição/main.js
@@ -0,0 +1,52 @@
+// Laço while do Meu
+let profHelo = 1;
+
+// while (profHelo <=5){
+//     console.log('Bater Palmas');
+//     // profHelo = profHelo + 1
+//     profHelo++ //loop infinito sem incrimento ou decremento
+// }
+
+//Exibir o valor de 1 até 10
+
+// let contador = 1;
+
+// while (contador <=10) {
+//  console.log(`O Valor do contador é ${contador}`)
+//  contador++
+// }
+
+// //Decremento
+// let contador2 = 10
+
+// while (contador2 >= 1) {
+//   console.log(`O valor do contador diminuindo é ${contador2}`)
+//   contador2--
+// }
+
+// // do while
+// do {
+//   console.log("Bater Palmas");
+//   console.log(profHelo);
+//   profHelo++;
+// } while (profHelo <= 5);
+
+// Laços For
+// for(let Zapzip = 1; Zapzip <= 5; Zapzip++){
+//  console.log(`Olá Zipzap`)
+// }
+
+// for(let Zipzap = 10; Zipzap >=1; Zipzap--){
+//   console.log (`Olá Zapzip ${Zipzap}`)
+// }
+
+//Laços de repetição e Array
+// const frutas = ["maca", "laranja", "pera", 10];
+
+// for (let j = 0; j < frutas.length; j++) {
+//   console.log(`Nome; ${frutas[j]}`);
+// }
+
+const carros = ['BMW', 'mitsubishi', 'Ford', 'Honda', 'Jeep', 'Porsche', 'Tesla', 'Suzuki'];
+for (let i = 0; i < carros.length; i= i+2)
+console.log(`Marcas de carros; ${carros[i]}`);
