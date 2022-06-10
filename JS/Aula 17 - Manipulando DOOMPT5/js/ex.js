42bbfe59d2d9858c517302f7b7447b387a441c9c
Matheus G <maat.teteu@hotmail.com>
2022-06-09 21:43:03 -0300

    Exercicio 15 finalizando

--- JS/Aula 17 - Manipulando DOOMPT5/js/ex.js
+++ JS/Aula 17 - Manipulando DOOMPT5/js/ex.js
@@ -1,4 +1,20 @@
-
+let addValor = document.getElementById('addvalue')
+let addQuantidade = document.getElementById('addvalue')
+let addItems = document.getElementById('addAmount')
 let itemList = document.getElementById("items");
 let preço = document.getElementById("value")
 let quantidade = document.getElementById("amount")
+
+addItems.addEventListener("click", adicionarItems)
+
+ 
+
+function adicionarItems(e){
+
+}
+
+function criarElementosLi(){
+    let liNomeItem = document.createElement("li")
+    let liQuantidade = document.createElement("li")
+    let liValor = document.createElement("li")
+}
